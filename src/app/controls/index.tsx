788439f8ef8a7366e8647342645986c246f38f0c
Sarah <srenfro00@gmail.com>
2022-02-26 17:05:31 -0600

    Add startOverButton

--- src/app/controls/index.tsx
+++ src/app/controls/index.tsx
@@ -1,9 +1,11 @@
-import ColorModeButton from './colorMode'
 import AutoSolve from './autoSolve'
+import ColorModeButton from './colorMode'
+import StartOver from './startOver'
 import { space } from '../../theme'
 
 const _ = () => (
   <div style={{ margin: space[1], display: 'flex' }}>
+    <StartOver />
     <AutoSolve />
     <ColorModeButton />
   </div>
---
+++ src/app/controls/startOver.tsx
@@ -0,0 +1,18 @@
+import { FaSolidSyncAlt } from 'solid-icons/fa'
+import { dispatch } from '../../store'
+import { resetBoard } from '../../store/actions'
+import { colorModeValue, colors } from '../../theme'
+import { style } from './style'
+
+const _ = () => {
+  const onClick = () => dispatch(resetBoard)
+  const color = colorModeValue(colors.black, colors.white)
+  const background = colorModeValue(colors.gray[300], colors.gray[700])
+  return (
+    <div style={style(background, color)} onClick={onClick}>
+      <FaSolidSyncAlt size={20} />
+    </div>
+  )
+}
+
+export default _
